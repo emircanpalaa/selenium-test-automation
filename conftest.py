241b862b685752ab85c0b71c85b29b2f99496e62
emircanpalaa <emopala9@gmail.com>
2026-05-23 23:10:40 +0300

    Add parallel test execution support

--- conftest.py
+++ conftest.py
@@ -1,23 +1,50 @@
 import os
 import pytest
+
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        action="store",
+        default="chrome",
+        choices=["chrome", "firefox"],
+        help="Browser seçimi: chrome veya firefox"
+    )
 
 
 @pytest.fixture
-def driver():
-    options = Options()
+def driver(request):
+    browser = request.config.getoption("--browser")
 
-    options.add_experimental_option("prefs", {
-        "credentials_enable_service": False,
-        "profile.password_manager_leak_detection": False
-    })
+    if browser == "chrome":
+        options = ChromeOptions()
 
-    options.add_argument("--disable-save-password-bubble")
-    options.add_argument("--headless=new")
-    options.add_argument("--window-size=1920,1080")
+        options.add_experimental_option("prefs", {
+            "credentials_enable_service": False,
+            "profile.password_manager_enabled": False,
+            "profile.password_manager_leak_detection": False
+        })
 
-    driver = webdriver.Chrome(options=options)
+        options.add_argument("--disable-save-password-bubble")
+        options.add_argument("--headless=new")
+        options.add_argument("--window-size=1920,1080")
+
+        driver = webdriver.Chrome(options=options)
+
+    elif browser == "firefox":
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        options.add_argument("--width=1920")
+        options.add_argument("--height=1080")
+
+        driver = webdriver.Firefox(options=options)
+
+    else:
+        raise ValueError(f"Unsupported browser: {browser}")
 
     driver.get("https://www.saucedemo.com/")
 
@@ -25,14 +52,17 @@ def driver():
 
     driver.quit()
 
+
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     outcome = yield
     rep = outcome.get_result()
 
-    if rep.when == "call" and rep.failed:
-        driver = item.funcargs['driver']
+    if rep.when == "call" and rep.failed and "driver" in item.funcargs:
+        driver = item.funcargs["driver"]
+
         os.makedirs("screenshots", exist_ok=True)
+
         screenshot_path = os.path.join("screenshots", f"{item.name}.png")
-        
+
         driver.save_screenshot(screenshot_path)
